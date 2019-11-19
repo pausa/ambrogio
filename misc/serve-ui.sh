b1511e3056bb5db350e47de1a4be499891f1898e
pausa <pausatep@gmail.com>
2019-11-19 19:52:32 +0100

    added script to launch clock on rpi

---
+++ misc/serve-ui.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export DISPLAY=:0.0
+cd "`dirname $0`/.."
+unclutter -idle 0 &
+npm run serve ui/dist &
+sleep 2
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+chromium-browser --disable-infobars --kiosk 'http://localhost:5000'
