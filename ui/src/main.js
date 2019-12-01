73b7d16f5693cdbcd9d4294361cfbe81f3ea6d6f
pausa <pausatep@gmail.com>
2019-12-01 19:26:07 +0100

    added notification server. Migrated to electron app

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,24 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-var ipc = require("node-ipc");
-ipc.config.id = 'ambrogio-ui';
-ipc.config.retry = 1500;
-ipc.serve(
-    function () {
-        ipc.server.on(
-            'notification',
-            function (data, sock) {
-                ipc.log("hello workd");
-                ipc.log(data);
-                ipc.log(sock);
-                this.socket = 'connected';
-            }
-        )
-    }
-);
-ipc.server.start();
-
 new Vue({
     render: h => h(App),
 }).$mount('#app');
