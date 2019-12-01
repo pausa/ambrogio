6e40fa3ec8bebb2b58498108323c7ef66fba3c9a
pausa <pausatep@gmail.com>
2019-12-01 20:38:50 +0100

    worked around issue with serve... need to find a better solution

--- misc/serve-ui.sh
+++ misc/serve-ui.sh
@@ -4,6 +4,8 @@ cd "`dirname $0`/.."
 unclutter -idle 0 &
 npm run serve ui/dist &
 sleep 2
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-chromium-browser --disable-infobars --kiosk 'http://localhost:5000'
+#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+#chromium-browser --disable-infobars --kiosk 'http://localhost:5000'
+cd ui
+npm run app
--- ui/app.js
+++ ui/app.js
@@ -7,7 +7,9 @@ let win;
 if (process.env.NODE_ENV === 'DEV') {
   url = 'http://localhost:8080/';
 } else {
+  console.log(process.cwd());
   url = `file://${process.cwd()}/dist/index.html`;
+  url = 'http://localhost:5000/';
 }
 
 app.on('ready', () => {
