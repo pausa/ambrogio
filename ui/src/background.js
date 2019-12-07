b79684e50d7b748512a343f7985cbe65b6c3ded0
pausa <pausatep@gmail.com>
2019-12-07 18:56:46 +0100

    added vue electron builder

---
+++ ui/src/background.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, protocol } = require('electron');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { createProtocol } = require('vue-cli-plugin-electron-builder/lib');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+let win;
+
+function loadFront(win) {
+  if (isDevelopment) {
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+  } else {
+    createProtocol('app');
+    win.loadFile('index.html');
+  }
+}
+
+app.on('ready', () => {
+  win = new BrowserWindow({
+    width: 320,
+    height: 240,
+    kiosk: true,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  loadFront(win);
+  if (isDevelopment) {
+    win.webContents.openDevTools();
+  }
+});
+
+const rest = express();
+rest.use(bodyParser.urlencoded({ extended: false }));
+rest.use(bodyParser.json());
+rest.post('/notification', function (req, res) {
+  let arg = {};
+  arg.type = 'notification';
+  arg.body = req.body;
+  win.webContents.send('new-component', arg);
+  res.sendStatus(200);
+});
+
+rest.listen(3000);
+
